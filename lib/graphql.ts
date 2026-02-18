f84fb1181e10b8b14372ac24f8c1c2305c10d150
Ariadna Ballestar <ariadna.ballestar.mateo@gmail.com>
2026-02-18 20:57:52 +0100

    feat: add on-demand revalidation webhook

--- lib/graphql.ts
+++ lib/graphql.ts
@@ -1,3 +1,9 @@
 import { GraphQLClient } from "graphql-request";
 
-export const client = new GraphQLClient(process.env.NEXT_PUBLIC_WP_API!);
+export const client = new GraphQLClient(process.env.NEXT_PUBLIC_WP_API!, {
+  fetch: (url, options) =>
+    fetch(url, {
+      ...options,
+      next: { tags: ['wordpress-content'] },
+    }),
+});
